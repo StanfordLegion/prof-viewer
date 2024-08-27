19facae30d5373206042e230a7bdbac623e1a8f7
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-08-27 16:04:14 -0700

    Fix the build.

--- src/strip_data.rs
+++ src/strip_data.rs
@@ -1,18 +1,15 @@
 use crate::data::{
-    DataSourceDescription, DataSourceInfo, EntryID, EntryIndex, EntryInfo, Field, ItemLink,
-    ItemUID, SlotMetaTile, SlotTile, SummaryTile, TileID, DataSource,
+    DataSource, DataSourceDescription, DataSourceInfo, EntryID, Field, ItemLink, SlotMetaTile,
+    SlotTile, SummaryTile, TileID,
 };
 
 pub struct StripDataSource<T: DataSource> {
     data_source: T,
 }
 
-
 impl<T: DataSource> StripDataSource<T> {
     pub fn new(data_source: T) -> Self {
-        Self {
-            data_source,
-        }
+        Self { data_source }
     }
 }
 
@@ -33,21 +30,33 @@ impl<T: DataSource> DataSource for StripDataSource<T> {
         self.data_source.fetch_slot_tile(entry_id, tile_id, full)
     }
 
-    fn fetch_slot_meta_tile(&self, entry_id: &EntryID, tile_id: TileID, full: bool) -> SlotMetaTile {
-        let mut tile = self.data_source
+    fn fetch_slot_meta_tile(
+        &self,
+        entry_id: &EntryID,
+        tile_id: TileID,
+        full: bool,
+    ) -> SlotMetaTile {
+        let mut tile = self
+            .data_source
             .fetch_slot_meta_tile(entry_id, tile_id, full);
         for row in &mut tile.data.items {
             for item in row {
                 item.title = "Redacted".to_string();
-                for field in item.fields {
+                for field in &mut item.fields {
                     match field.1 {
-                        Field::I64(_) => {},
-                        Field::U64(_) => {},
-                        Field::String(x) => { *x = "Redacted".to_string(); },
-                        Field::Interval(_) => {},
-                        Field::ItemLink(ItemLink { title, .. }) => { *title = "Redacted".to_string(); },
-                        Field::Vec(_) => { todo!() },
-                        Field::Empty => {},
+                        Field::I64(_) => {}
+                        Field::U64(_) => {}
+                        Field::String(ref mut x) => {
+                            *x = "Redacted".to_string();
+                        }
+                        Field::Interval(_) => {}
+                        Field::ItemLink(ItemLink { ref mut title, .. }) => {
+                            *title = "Redacted".to_string();
+                        }
+                        Field::Vec(_) => {
+                            todo!()
+                        }
+                        Field::Empty => {}
                     }
                 }
             }
