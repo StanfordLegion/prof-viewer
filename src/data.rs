bd70ac325949ce9a4c9d04c84774673996a8f2e9
Elliott Slaughter <slaughter@cs.stanford.edu>
2025-05-12 14:02:10 -0700

    Add default for warning message.

--- src/data.rs
+++ src/data.rs
@@ -25,6 +25,7 @@ pub struct DataSourceInfo {
     pub interval: Interval,
     pub tile_set: TileSet,
     pub field_schema: FieldSchema,
+    #[serde(default)]
     pub warning_message: Option<String>,
 }
 
